2d27bd065c9ecba0a00eec3fd8a3b2576b640194
trvrenglish <trvrenglish@gmail.com>
2023-09-26 16:56:53 -0700

    form input field renders only last 10

--- Django/number_app/number_app/views.py
+++ Django/number_app/number_app/views.py
@@ -2,13 +2,16 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import NumberInput
 from .models import NumberForm
-from django import forms
 
 def renderPage(request):
-    mynumbers = NumberInput.objects.all().values()
+    mynumbers = NumberInput.objects.all().order_by('-id')[:10]
     template = loader.get_template('home.html')
     context = {
         'mynumbers': mynumbers,
     }
     context['form']= NumberForm()
+
+    if request.method == 'POST':
+        number = NumberInput(number = request.POST.get('number'))
+        number.save()
     return HttpResponse(template.render(context, request))
