0656e8b3a135232785f046fdcd4e0555181c86df
Donovan <dhowat@westmont.edu>
2023-10-15 11:19:14 -0700

    dhowat Phase 2

---
+++ dhowat-phase2/src/createschema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE person (
+  id integer,
+  fname varchar(30),
+  lname varchar(30),
+  email varchar(40),
+  phone varchar(13),
+  gender varhar(10)
+);
+
+CREATE TABLE faculty (
+  faculty_id integer,
+  tenured boolean,
+  term_begin date,
+  term_end date
+);
+
+CREATE TABLE degree (
+  degree_id integer,
+  degree_name varchar(30),
+  degree_type varchar(30),
+  year_grad integer
+);
+
+CREATE TABLE rank (
+  faculty_id integer,
+  instructor boolean,
+  professor boolean,
+  associate_professor boolean,
+  assistant_professor boolean
+);
+
+CREATE TABLE university (
+  university_id integer,
+  highest_level varchar(40),
+  christian_affiliation boolean
+);
