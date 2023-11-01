86f9e7cf87dd1fdedf74991d435696b2f2903aab
trvrenglish <trvrenglish@gmail.com>
2023-10-31 19:36:29 -0700

    c

--- tenglish-phase2/src/c.sql
+++ tenglish-phase2/src/c.sql
@@ -1,25 +1,25 @@
 -- Create a view to display faculty and their degree information
-CREATE VIEW faculty_degrees_view AS
+CREATE VIEW testview3 AS
 SELECT
-    f.id AS faculty_id,
-    p.fname AS first_name,
-    p.lname AS last_name,
-    d.degree_name,
-    d.degree_type,
-    d.year_grad
-FROM faculty AS f
-JOIN person AS p ON f.person_id = p.id
-LEFT JOIN degree AS d ON f.id = d.faculty_id;
+    faculty.id AS faculty_id,
+    person.fname AS first_name,
+    person.lname AS last_name,
+    degree.degree_name,
+    degree.degree_type,
+    degree.year_grad
+FROM faculty
+JOIN person ON faculty.person_id = person.id
+LEFT JOIN degree ON faculty.id = degree.faculty_id;
 
 -- Create a view to display faculty members with their service hours and research publications
 CREATE VIEW faculty_service_research_view AS
 SELECT
-    f.id AS faculty_id,
-    p.fname AS first_name,
-    p.lname AS last_name,
-    COALESCE(cs.hrs, 0) AS service_hours,
-    COALESCE(r.number_of_publications, 0) AS publications
-FROM faculty AS f
-JOIN person AS p ON f.person_id = p.id
-LEFT JOIN community_service AS cs ON f.id = cs.faculty_id
-LEFT JOIN research AS r ON f.id = r.faculty_id;
+    faculty.id AS faculty_id,
+    person.fname AS first_name,
+    person.lname AS last_name,
+    COALESCE(community_service.hrs, 0) AS service_hours,
+    COALESCE(research.number_of_publications, 0) AS publications
+FROM faculty AS faculty
+JOIN person AS person ON faculty.person_id = person.id
+LEFT JOIN community_service AS community_service ON faculty.id = community_service.faculty_id
+LEFT JOIN research AS research ON faculty.id = research.faculty_id;
