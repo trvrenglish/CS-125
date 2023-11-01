8aabcf7ebf53b4004c317aea1b83a51a6f31795a
Hannah DeSoto <hannahdesotom@gmail.com>
2023-10-31 19:40:06 -0700

    drop schema

--- tenglish-phase2/src/dropschema.sql
+++ tenglish-phase2/src/dropschema.sql
@@ -1,1 +1,9 @@
-DROP TABLE person, faculty, degree, rank, university;
+
+-- Add CASCADE option for dropping tables with foreign key constraints
+DROP TABLE IF EXISTS community_service CASCADE;
+DROP TABLE IF EXISTS research CASCADE;
+DROP TABLE IF EXISTS university CASCADE;
+DROP TABLE IF EXISTS degree CASCADE;
+DROP TABLE IF EXISTS faculty_rank CASCADE;
+DROP TABLE IF EXISTS faculty CASCADE;
+DROP TABLE IF EXISTS person CASCADE;
